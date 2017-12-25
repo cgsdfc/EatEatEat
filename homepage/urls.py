4f3cd45567a225fdef664294d94479eb37524d1d
cgsdfc <cgsdfc@126.com>
2017-12-26 02:34:27 +0800

    Now had a index and a link to display dishes!

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from .views import index
+from .views import DishesListView
 
 urlpatterns=[
         path('', index),
+        path('dishes/', DishesListView.as_view(), name='dishes'),
         ]
 
--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Dish, Restaurant
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 def index(request):
     dishes = Dish.objects.all()
-    print(dishes)
     return render(request, 'homepage/index.html', context={ 'dishes': dishes })
+
+class DishesListView(ListView):
+    model=Dish
+
+class DishesDetailView(DetailView):
+    model=Dish
 
 
